a4ad3cb05ea0b1c03777aac07df08b6cdca5f008
mikemel21 <mikemelei905@gmail.com>
2024-07-14 19:23:38 -0500

    First Commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    testNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(testNode)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,18 @@
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    '''return True if all properties of two TextNode objects are equal.'''
+    def __eq__(self, otherNode):
+        return self.text == otherNode.text and self.text_type == otherNode.text_type and self.url == otherNode.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def main():
+    testNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(testNode)
+
+if __name__ == "__main__":
+    main()
